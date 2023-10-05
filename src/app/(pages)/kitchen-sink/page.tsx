4a084a81f3f7bf3a53b7ac1a94b46c7fa9876fdc
Farhan Farizan <farhan.farizan@fuel.com.my>
2023-10-05 22:01:41 +0800

    TIME TO REST

--- src/app/(pages)/kitchen-sink/page.tsx
+++ src/app/(pages)/kitchen-sink/page.tsx
@@ -4,14 +4,14 @@ import styles from "../kitchen-sink/kitchen-sink.module.scss";
 const KitchenSink = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.containerGrid}>
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-        <div>4</div>
-        <div>5</div>
-        <div>6</div>
-        <div>7</div>
+      <div className={styles.mainBox}>
+        <div className={styles.sectionOne}></div>
+        <div className={styles.sectionTwo}>
+          <div className={styles.smallbox}></div>
+          <div className={styles.smallbox}></div>
+        </div>
+        <div className={styles.sectionThree}></div>
+        <div className={styles.sectionFour}></div>
       </div>
     </div>
   );
